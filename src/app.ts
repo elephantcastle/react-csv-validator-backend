f4f9600e807b176521b2fdb12d8ba278fadf35a6
elephantcastle <obymik89@gmail.com>
2021-04-05 18:48:26 +0200

    setup route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import express from "express";
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.post('/validate-csv', (req, res) => {
+
+})
+
+
+
+
+
 
 app.listen(5000, () => {
   console.log("> started on port 5000");
